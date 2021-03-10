e6bf36e9d4ee5385248af5b14ceb2d7dd1e7b53c
Brandy Quinlan <brandyquinlan.bootcamp@gmail.com>
2021-03-10 12:50:03 -0600

    commit schema and seeds files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+use burgers_db;
+INSERT INTO  (burger_name) VALUES ('Cheeseburger Deluxe');
+INSERT INTO  (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO  (burger_name) VALUES ('Double Cheeseburger');
+INSERT INTO  (burger_name, devoured) VALUES ('Big mac', false);
+
